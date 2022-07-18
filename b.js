9acf883e80ac251a13ed56a64e2fa8bbb682b3e3
espirapaco23 <102536675+espirapaco23@users.noreply.github.com>
2022-07-18 16:30:34 +0300

    resume

---
+++ b.js
@@ -0,0 +1,13 @@
+VANTA.BIRDS({
+  el: "#intro",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+
+  scale: 1.0,
+  scaleMobile: 1.0,
+  backgroundColor: 0x9d2189,
+  color1: 0xcdb628,
+  color2: 0x9f4cb9,
+  colorMode: "lerpGradient",
+});
